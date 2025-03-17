2a7e3278ba172c61f5449830032810f767064215
ereyes0212 <erick.reyes@tiempo.hn>
2025-03-17 16:45:37 -0600

    Update IRolesService.cs

--- interfaces/Rol/IRolesService.cs
+++ interfaces/Rol/IRolesService.cs
@@ -1,7 +1,14 @@
-﻿namespace Inventario.interfaces.Rol
+﻿using Inventario.models.Rol;
+
+namespace Inventario.interfaces.Rol
 {
     public interface IRolesService
     {
-        
+        Task<IEnumerable<RolDto>> GetRoles();
+        Task<IEnumerable<RolDto>> GetRolesActivos();
+        Task<RolDto> GetRolById(string id);
+        Task<RolDto> PostRol(Role rol);
+        Task<RolDto> PutRol(Role rol, string id);
+        Task AssignPermissionsToRole(string roleId, List<string> ids);
     }
 }
